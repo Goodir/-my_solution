a88979631176dd6cadfc155718f5aed13ab5f4e0
Anton Sotnikov <sotnikovantn@gmail.com>
2018-05-19 23:55:23 +0300

    Added FrogJmp

---
+++ FrogJmp.py
@@ -0,0 +1,34 @@
+"""
+Write a function:
+
+def solution(X, Y, D)
+
+that, given three integers X, Y and D,
+returns the minimal number of jumps from position X to a position equal to or greater than Y.
+
+For example, given:
+
+  X = 10
+  Y = 85
+  D = 30
+the function should return 3, because the frog will be positioned as follows:
+
+after the first jump, at position 10 + 30 = 40
+after the second jump, at position 10 + 30 + 30 = 70
+after the third jump, at position 10 + 30 + 30 + 30 = 100
+Assume that:
+
+X, Y and D are integers within the range [1..1,000,000,000];
+X ≤ Y.
+Complexity:
+
+expected worst-case time complexity is O(1);
+expected worst-case space complexity is O(1).
+"""
+
+
+def solution(X, Y, D):
+    a = (Y - X) / D
+    if not (a.is_integer()):
+        a += 1
+    return int(a)
